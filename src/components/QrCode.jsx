9a0d6186b923de9e531f08746ba1b7d26f09ea3c
Shankarwal <sk9255543@gmail.com>
2023-02-26 03:28:48 +0530

    Improved look

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -5,13 +5,13 @@ const QrCode = () => {
     <Wrapper>
       <article className="code-box">
         <div className="img-container">
-          <img src="/images/qr-code.png" />
+          <img src="/images/qr-code.png" alt="qr-code" />
         </div>
         <div className="about">
-          <h2>improve your front-End skills by building projects</h2>
+          <h2>Improve your front-end skills by building projects</h2>
           <p>
             Scan the QR code to visit Frontend Mentor and take your front-end
-            skills to next level
+            skills to the next level
           </p>
         </div>
       </article>
@@ -28,7 +28,7 @@ const Wrapper = styled.div`
   .code-box {
     text-align: center;
     width: 85vw;
-    max-width: 40rem;
+    max-width: 30rem;
     height: auto;
     padding: 2.4rem;
     background: var(--White);
@@ -37,41 +37,30 @@ const Wrapper = styled.div`
     flex-direction: column;
     gap: 2rem;
     .img-container img {
-      /* width: 30rem;
-      height: 30rem; */
       width: 100%;
       border-radius: 1.2rem;
     }
   }
   .about {
     h2 {
-      font-size: 2.2rem;
-      letter-spacing: 0.15rem;
-      margin-bottom: 1.6rem;
+      font-size: 2rem;
+      margin-bottom: 1.4rem;
+      color: var(--Dark-blue);
     }
     p {
-      font-size: 1.8rem;
-      letter-spacing: 0.1rem;
-      font-weight: 400;
-      opacity: 0.7;
+      font-size: 1.5rem;
+      margin-bottom: 1rem;
+      color: var(--Grayish-blue);
     }
   }
-  @media (max-width: 1440px) {
-    .code-box {
-      max-width: 36rem;
-    }
+  @media (max-width: 324px) {
     .about {
       h2 {
-        font-size: 2rem;
-        letter-spacing: 0.1rem;
+        font-size: 1.7rem;
         margin-bottom: 1.6rem;
       }
       p {
-        font-size: 1.6rem;
-        letter-spacing: 0.1rem;
-        font-weight: 400;
-        opacity: 0.7;
-        margin-bottom: 2.4rem;
+        font-size: 1.3rem;
       }
     }
   }
